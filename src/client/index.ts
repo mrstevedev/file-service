fb58a4ab45771b63962c7a1514d1d3184f34a5ac
Steven <stevendotpulido@gmail.com>
2024-12-24 01:13:00 -0800

    chore: replace with ENV vars

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
-export const aws_s3_bucket = "user-service-s3-bucket";
-export const aws_s3_region = "us-west-1";
+export const aws_s3_bucket = process.env.AWS_S3_BUCKET;
+export const aws_s3_region = process.env.AWS_S3_REGION;
 export const aws_s3_access_key = process.env.AWS_S3_ACCESS_KEY;
 export const aws_s3_secret_key = process.env.AWS_S3_SECRET_KEY;
 
