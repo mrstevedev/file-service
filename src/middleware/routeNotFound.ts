d0759583711549be39475d19a2ea421b6a953385
Steven <stevendotpulido@gmail.com>
2024-12-24 22:12:01 -0800

    refactor: conditional removed and logging error changed to ROUTE_NOT_FOUND

--- src/middleware/routeNotFound.ts
+++ src/middleware/routeNotFound.ts
@@ -2,10 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { ERROR } from "../constants";
 
 export function routeNotFound(request: Request, response: Response, next: NextFunction) {
-    const error = new Error(ERROR.ROUTE_NOT_FOUND);
-    if (error) {
-        logging.error(error);
-        response.status(404).json({ error: error.message });
-    }
+    response.status(404);
+    logging.error(ERROR.ROUTE_NOT_FOUND);
     next();
 }
