d9ee0d14654509a453944b8eb5aadc4f894a7f87
Steven <stevendotpulido@gmail.com>
2024-12-24 22:00:12 -0800

    feat: add ORIGIN url constant

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,6 +8,8 @@ export const TEST = process.env.NODE_ENV === "test";
 export const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || "localhost";
 export const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 12345;
 
+export const ORIGIN = process.env.ORIGIN || "http://localhost:3000";
+
 export const server = {
     SERVER_HOSTNAME,
     SERVER_PORT
